cc0f08e9dc6f505f8aa049cb834b1ffb7ad66bc6
Sabeel Khan <sabeel.khan@Sabeel-ka-MacBook-Pro.local>
2026-06-15 23:21:51 +0530

    Kanban Board V1

--- Javascript/Class-20/Kanban-Board-v1/script.js
+++ Javascript/Class-20/Kanban-Board-v1/script.js
@@ -1,8 +1,11 @@
 const todoTaskContainer = document.querySelector('.todo-tasks');
 const inprogressTaskContainer = document.querySelector('.inprogress-tasks');
 const doneTaskContainer = document.querySelector('.done-tasks');
-
-
+const form = document.querySelector('form');
+const addTaskBtn = document.querySelector('#add-task-btn');
+const modalContainer = document.querySelector('.modal-container');
+const closeModalBtn = document.querySelector('#close-modal-btn');
+const taskBoard = document.querySelector('.task-board');
 // https://stackoverflow.com/questions/105034/how-do-i-create-a-guid-uuid
 function uuid() {
   return "10000000-1000-4000-8000-100000000000".replace(/[018]/g, c =>
@@ -51,10 +54,11 @@ const tasks = [
 ];
 
 function createTaskUI(task) {
-    return `<div class="task">
+    return `<div class="task" data-priority="${task.priority}" data-id="${task.id}">
         <p>${task.desc}</p>
         <p>${task.status}</p>
         <p>${task.priority }</p>
+        <i class="bi bi-trash"></i>
     </div>`
 }
 
@@ -82,8 +86,58 @@ function renderTasks() {
     renderListToUI(doneTaskContainer, doneTasks);
 }
 
+function addTask(task) {
+    tasks.push(task);
+    renderTasks();
+}
+
+function deleteTask(idToBeDeleted) {
+    const taskIndex = tasks.findIndex((task) => task.id === idToBeDeleted);
+    if (taskIndex === -1) {
+        throw new Error('Invalid id');
+    }
+
+    tasks.splice(taskIndex, 1);
+    renderTasks();
+}
+
+function registerEventListener() {
+    form.addEventListener('submit', function (event) {
+        event.preventDefault();
+        const taskDesc = form.elements[0].value;
+        const priorityInp = document.querySelector('.add-task-modal form input:checked');
+
+        const newTask = {
+            id: uuid(),
+            desc: taskDesc,
+            status: 'todo',
+            priority: priorityInp.value,
+            dateCreated: new Date().toISOString()
+        }
+        addTask(newTask);
+        modalContainer.style.display = 'none'
+    });
+
+    addTaskBtn.addEventListener('click', function () {
+        modalContainer.style.display = 'block'
+    });
+
+    closeModalBtn.addEventListener('click', function () {
+        modalContainer.style.display = 'none'
+    });
+
+    taskBoard.addEventListener('click', function (event) {
+        if (event.target.nodeName !== 'I') {
+            return;
+        }
+        const taskIdToBeDeleted = event.target.parentElement.getAttribute('data-id');
+        deleteTask(taskIdToBeDeleted);
+    })  
+}
+
 function initializeApp() {
     renderTasks();
+    registerEventListener()
 }
 
 initializeApp();
